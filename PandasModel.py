4a17e58d6cc35213ca61d9a98ece0db9e5aa3138
donutyeon <y606800@gmail.com>
2022-10-12 22:02:17 +0100

    added ui, upload, save and info

---
+++ PandasModel.py
@@ -0,0 +1,61 @@
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+import pandas as pd
+
+class PandasModel(QtCore.QAbstractTableModel): 
+    def __init__(self, df = pd.DataFrame(), parent=None): 
+        QtCore.QAbstractTableModel.__init__(self, parent=parent)
+        self._df = df
+
+    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+        if role != QtCore.Qt.DisplayRole:
+            return QtCore.QVariant()
+
+        if orientation == QtCore.Qt.Horizontal:
+            try:
+                return self._df.columns.tolist()[section]
+            except (IndexError, ):
+                return QtCore.QVariant()
+        elif orientation == QtCore.Qt.Vertical:
+            try:
+                # return self.df.index.tolist()
+                return self._df.index.tolist()[section]
+            except (IndexError, ):
+                return QtCore.QVariant()
+    
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if role != QtCore.Qt.DisplayRole:
+            return QtCore.QVariant()
+
+        if not index.isValid():
+            return QtCore.QVariant()
+
+        return QtCore.QVariant(str(self._df.iloc[index.row(), index.column()]))
+
+    def setData(self, index, value, role):
+        if role == Qt.EditRole:
+            self._df.iloc[index.row(),index.column()] = value
+            return True
+            
+    def flags(self, index):
+        fl = super(self.__class__,self).flags(index)
+        fl |= Qt.ItemIsEditable
+        fl |= Qt.ItemIsSelectable
+        fl |= Qt.ItemIsEnabled
+        fl |= Qt.ItemIsDragEnabled
+        fl |= Qt.ItemIsDropEnabled
+        return fl
+
+    def rowCount(self, parent=QtCore.QModelIndex()): 
+        return len(self._df.index)
+
+    def columnCount(self, parent=QtCore.QModelIndex()): 
+        return len(self._df.columns)
+
+    # def sort(self, column, order):
+    #     colname = self._df.columns.tolist()[column]
+    #     self.layoutAboutToBeChanged.emit()
+    #     self._df.sort_values(colname, ascending= order == QtCore.Qt.AscendingOrder, inplace=True)
+    #     self._df.reset_index(inplace=True, drop=True)
+    #     self.layoutChanged.emit()
---
+++ projet.py
@@ -0,0 +1,143 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
+from PyQt5.QtWidgets import QFileDialog
+from PandasModel import PandasModel
+import sys
+import ctypes
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('PROJET_DM.ui', self)
+        ######################################### items declaration #######################################################
+        self.upload_button= self.findChild(QtWidgets.QPushButton,'upload_button')
+        self.upload_button.clicked.connect(self.UploadClickListener)
+
+        self.choose_column= self.findChild(QtWidgets.QPushButton,'choose_column')
+        self.choose_column.clicked.connect(self.ColumnClickListener)
+
+        self.save_button= self.findChild(QtWidgets.QPushButton,'save_button')
+        self.save_button.clicked.connect(self.SaveClickListener)
+        
+
+        self.dataset_info= self.findChild(QtWidgets.QLabel,'dataset_info')
+        self.column_info= self.findChild(QtWidgets.QLabel,'column_info')
+
+        self.pandasTv=self.findChild(QtWidgets.QTableView,'pandasTv')
+        self.pandasTv.setSortingEnabled(True)
+        self.show()
+
+
+    def UploadClickListener(self):
+        self.path = QFileDialog.getOpenFileName(self, "Import CSV", "", "CSV data files (*.csv)")
+        # path = QFileDialog.getOpenFileName(self, 'Open a file', '','All Files (*.*)') # if we want all files
+        self.df = pd.read_csv(self.path[0])
+        info="Nombre de lignes: "+str(self.df.shape[0])+"\nNombre de colonnes: "+str(self.df.shape[1])
+        print(info)
+        #self.dataset_info.setText(info)
+        self.pandasTv_model = PandasModel(self.df)
+        self.dataset_info.setText(info)
+        self.pandasTv.setModel(self.pandasTv_model)
+        self.pandasTv.clicked[QtCore.QModelIndex].connect(self.ColumnClickListener)
+        if self.path != ('',''):
+            print(self.path[0])
+            return self.path[0]
+
+    def SaveClickListener(self):
+        try:
+            self.df.to_csv(self.path[0], index=False)
+        except:
+            ctypes.windll.user32.MessageBoxW(0, "No dataset loaded.", "Error!", 0)
+
+    def ColumnClickListener(self,item):
+        print(item.column())
+        print(self.df.dtypes[self.df.columns[item.column()]])
+        if (self.df.dtypes[self.df.columns[item.column()]] != 'object'):
+            td=tendanceCentrale(self.df[self.df.columns[item.column()]])
+            dis=dispersion(self.df[self.df.columns[item.column()]])
+            q=dis[0][4]
+            if len(dis[1])!=0:
+                outliers=str(dis[1])
+            else:
+                outliers="no outliers"
+            info="Column name: "+str(self.df.columns[item.column()])+"\nColumn type: "+str(self.df.dtypes[self.df.columns[item.column()]])+"\nMean: "+str(td['mean'])+"\nMedian: "+str(td['median'])+"\nMode: "+str(td['mode'])+"\nSymmetry: "+td['symetrie']+"\nEcart type: "+str(dis[0][0])+"\nVariance: "+str(dis[0][1])+"\nMin: "+str(dis[0][3])+"\nQ1: "+str(q[0])+"\nQ3: "+str(q[1])+"\nMax: "+str(dis[0][5])+"\nIQR: "+str(dis[0][2])+"\nOutliers: "+outliers
+            #print(info)
+            self.column_info.setText(info)
+        else:
+            info="Column name: "+str(self.df.columns[item.column()])+"\nColumn type: "+str(self.df.dtypes[self.df.columns[item.column()]])
+            print(info)
+            self.column_info.setText(info)
+
+def mean(df_column):
+    return(np.sum(df_column)/len(df_column))
+            
+def median(df_column):
+        df_column = df_column.sort_values(ascending=True).reset_index(drop=True)
+        if len(df_column) % 2 == 0:
+                return ((df_column[len(df_column)//2]+df_column[len(df_column)//2 - 1])/2)
+        else: return df_column[len(df_column)//2]
+
+def mode(df_column):
+        freq = {}
+        for elem in df_column:
+                if elem in freq.keys():
+                        freq[elem] += 1
+                else : freq[elem] = 1
+        return max(freq, key=freq.get)
+
+def tendanceCentrale(df_column): #retourne la moyenne, mode et mediane ainsi que la symétrie
+        tendances = []
+        mean_ = mean(df_column)
+        mode_ = mode(df_column)
+        median_ = median(df_column)
+        tendances.append(mean_)
+        tendances.append(mode_)
+        tendances.append(median_)
+        if round(mean_,1) == round(mode_,1) and round(mean_,1) == round(median_,1) and round(median_,1) == round(mode_,1):
+                tendances.append("Symétrique")
+        elif round(mean_,1) > round(median_,1) and round(median_,1) > round(mode_,1):
+                tendances.append("Positivement")
+        elif round(mean_,1) < round(median_,1) and round(median_,1) < round(mode_,1):
+                tendances.append("Négativement")
+        else: tendances.append("Inindentifiée")
+        return pd.Series(np.array(tendances), index=['mean', 'mode', 'median', 'symetrie'])
+
+def ecartType(df_column):
+    return(np.sqrt((np.sum(np.power(df_column-mean(df_column),2)))/len(df_column)))
+
+def variance(df_column):
+        return(np.power(ecartType(df_column),2))
+
+def getQuartiles(df_column):
+        df_column = df_column.sort_values(ascending=True).reset_index(drop=True)
+        return (df_column[len(df_column)//4], df_column[(len(df_column)//4)*3])
+        
+def ecartInterquartile(df_column):
+        Q1, Q3 = getQuartiles(df_column)
+        return Q3-Q1
+        
+def dispersion(df_column): #get outlier data and make it into a pandas series
+        mesures = []
+        standard_deviation = ecartType(df_column)
+        variance_ = variance(df_column)
+        inter_q = ecartInterquartile(df_column)
+        quart = getQuartiles(df_column)
+        mesures.append(standard_deviation)
+        mesures.append(variance_)
+        mesures.append(inter_q)
+        mesures.append(min(df_column))
+        mesures.append(quart)
+        mesures.append(max(df_column))
+        outliers = []
+        for each in df_column:
+                if each > quart[1]+1.5*inter_q or each < quart[0]-1.5*inter_q:
+                        outliers.append(each)
+        return (pd.Series(np.array(mesures), index=['ecart_type', 'variance', 'IQR', 'min','quartiles', 'max']), set(outliers))
+    
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
